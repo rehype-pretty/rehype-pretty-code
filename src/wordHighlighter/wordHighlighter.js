6f970eb8ca597c848bc3d2a31d3e57c54e8bd363
James N <cc.glows@gmail.com>
2022-02-20 12:46:55 +1100

    refactor: avoid Array.prototype.at() for old JS runtimes

--- src/wordHighlighter/wordHighlighter.js
+++ src/wordHighlighter/wordHighlighter.js
@@ -32,7 +32,7 @@ export function wordHighlighter(node, word, options, onVisitHighlightedWord) {
         wrapHighlightedWords(node, nodesToWrap, onVisitHighlightedWord);
         // re-start from the 'last' node (the word or part of it may exist multiple times in the same node)
         // account for possible extra nodes added from split with - 2
-        startIndex = Math.max(nodesToWrap.at(-1).index - 2, 0);
+        startIndex = Math.max(nodesToWrap[nodesToWrap.length - 1].index - 2, 0);
         textContent = node.children
           ?.map((childNode) => {
             if (
