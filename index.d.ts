5a07c250248f3ad25e20a8ade692b37edf925dab
atomiks <cc.glows@gmail.com>
2023-01-14 04:21:59 +1100

    fix(types): onVisitHighlightedWord accepts an id param

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 import {Highlighter} from 'shiki';
 
-type theme = JSON | string;
+type Theme = JSON | string;
 
 export type Options = {
-  theme: theme | Record<any, theme>;
+  theme: Theme | Record<any, Theme>;
   keepBackground: boolean;
   tokensMap: {[key: string]: string};
   filterMetaString: (string: string) => string;
   // TODO: strict types
   onVisitLine(node: any): void;
   onVisitHighlightedLine(node: any): void;
-  onVisitHighlightedWord(node: any): void;
-  getHighlighter?: (options: Pick<Options, 'theme'>) => Promise<Highlighter>;
+  onVisitHighlightedWord(node: any, id: string | undefined): void;
+  getHighlighter: (options: Pick<Options, 'theme'>) => Promise<Highlighter>;
 };
 
 declare const rehypePrettyCode: (options?: Partial<Options>) => any;
