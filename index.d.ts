a1ee413f261c2a3a82370b950f774c3a1087eb4a
atomiks <cc.glows@gmail.com>
2023-06-13 15:10:20 +1000

    fix: element type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import type {Highlighter} from 'shiki';
 import type {Transformer} from 'unified';
-import type {Root, Node} from 'hast';
+import type {Root, Element} from 'hast';
 
 type Theme = JSON | string;
 
@@ -9,9 +9,9 @@ export interface Options {
   keepBackground?: boolean;
   tokensMap?: Record<string, string>;
   filterMetaString?(str: string): string;
-  onVisitLine?(node: Node): void;
-  onVisitHighlightedLine?(node: Node): void;
-  onVisitHighlightedWord?(node: Node, id: string | undefined): void;
+  onVisitLine?(element: Element): void;
+  onVisitHighlightedLine?(element: Element): void;
+  onVisitHighlightedWord?(element: Element, id: string | undefined): void;
   getHighlighter?(options: Pick<Options, 'theme'>): Promise<Highlighter>;
 }
 
