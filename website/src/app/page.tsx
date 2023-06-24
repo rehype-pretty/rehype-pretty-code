024a5b8ec4a56318a6e6eac9221babce031fb851
atomiks <cc.glows@gmail.com>
2023-06-24 16:54:08 +1000

    docs: update

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -2,7 +2,7 @@ import Index from './index.mdx';
 
 export default function Home() {
   return (
-    <div className="prose prose-invert text-zinc-400 container px-4 mx-auto mt-12 mb-6">
+    <div className="prose prose-invert text-zinc-400 px-4 sm:px-6 md:px-8 mx-auto mt-12 mb-6">
       <h1 className="py-1 mx-auto mt-12 text-4xl sm:text-5xl md:text-6xl font-black text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500">
         Rehype Pretty Code
       </h1>
