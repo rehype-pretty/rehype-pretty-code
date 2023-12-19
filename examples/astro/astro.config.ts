700a3deac5eeee903b59e9b7034873b56f6b45a1
Omar <github@omaraziz.dev>
2023-12-18 18:17:05 -0800

    chore: cleanup

--- examples/astro/astro.config.ts
+++ examples/astro/astro.config.ts
@@ -1,11 +1,9 @@
 import mdx from '@astrojs/mdx';
-import rehypeSlug from 'rehype-slug';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import rehypePrettyCode from 'rehype-pretty-code';
 import moonlightTheme from './public/theme/moonlight-ii.json';
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: false,
@@ -13,17 +11,13 @@ export default defineConfig({
       [
         rehypePrettyCode,
         {
-          keepBackground: false,
           theme: moonlightTheme,
         },
       ],
-      rehypeSlug,
     ],
   },
   integrations: [
-    mdx({
-      extendMarkdownConfig: true,
-    }),
+    mdx(),
     tailwind(),
   ],
 });
