626bdfbb3e5cb19e281e338eff95acb23efdd5d7
Omar <github@omaraziz.dev>
2023-12-21 06:46:10 -0800

    feat: switch to bun

--- website/src/mdx-components.tsx
+++ website/src/mdx-components.tsx
@@ -9,7 +9,6 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
   };
 }
