ab71ae8c92752064b0d6581327b04459ecfedda5
atomiks <cc.glows@gmail.com>
2024-01-16 14:29:19 -0800

    fix: ignore math lang (#159)

--- src/index.ts
+++ src/index.ts
@@ -346,7 +346,7 @@ export default function rehypePrettyCode(
           defaultCodeBlockLang,
         );
 
-        if (!lang) return;
+        if (!lang || lang === 'math') return;
 
         const lineNumbers: number[] = [];
         if (meta) {
