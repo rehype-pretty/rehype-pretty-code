31ba46067007f72b39167a77a2eed62798ccb11c
omar <omaraziz.dev@proton.me>
2024-04-19 20:14:21 -0700

    changesets ci

---
+++ scripts/changeset-update-version.ts
@@ -0,0 +1,23 @@
+import bun from 'bun';
+
+/**
+ * This script is used by the `release.yml` workflow to update the version of the packages being released.
+ * The standard step is only to run `changeset version` but this does not update the lock file.
+ * So we also run `bun install`, which does this update.
+ * This is a workaround until this is handled automatically by `changeset version`.
+ * See https://github.com/changesets/changesets/issues/421.
+ */
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+async function run() {
+  const versionCommand = bun.$`pnpm exec changeset changeset version`.text();
+  const installCommand = bun.$`pnpm install --lockfile-only`.text();
+
+  const results = await Promise.all([versionCommand, installCommand]);
+
+  console.log(JSON.stringify(results, undefined, 2));
+}
