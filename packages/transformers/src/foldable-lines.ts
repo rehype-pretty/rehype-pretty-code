2aaa33372b8b30c5fc67bad7e25b5335768387c8
omar <omaraziz.dev@proton.me>
2024-05-13 19:21:04 -0700

    feat: foldable code lines transformer

--- packages/transformers/src/foldable-lines.ts
+++ packages/transformers/src/foldable-lines.ts
@@ -1,3 +1,4 @@
+import { splicer } from './utilities';
 import type { ShikiTransformer } from 'shiki';
 
 interface FoldableLinesOptions {
@@ -43,16 +44,96 @@ interface FoldableLinesOptions {
 export function transformerFoldableLines(
   _options: FoldableLinesOptions = {},
 ): ShikiTransformer {
+  const [lineFoldStart, lineFoldEnd] = [4, 10];
+
   return {
     name: '@rehype-pretty/transformers/foldable-lines',
+
     preprocess(code, _options) {
-      return code;
+      const [linesBeforeFold, foldLines, _linesAfterFold] = splicer(
+        code.split('\n'),
+        lineFoldStart,
+        lineFoldEnd,
+      );
+
+      console.log(foldLines);
+      // console.log(_options.meta)
+      return linesBeforeFold.join('\n');
     },
-    code(node) {
-      return node;
+    code(_node) {
+      // const [lineFoldStart, lineFoldEnd] = [1, 3];
+      // const [linesBeforeFold, foldLines, linesAfterFold] = splicer(
+      //   node.children,
+      //   lineFoldStart,
+      //   lineFoldEnd,
+      // );
+      // console.log(
+      //   node.children
+      // );
+      // node.children.splice(lineFoldStart + 1, (lineFoldEnd + 1) - lineFoldStart, {
+      //   type: 'element',
+      //   tagName: 'details',
+      //   properties: {},
+      //   children: [
+      //     {
+      //       type: 'element',
+      //       tagName: 'summary',
+      //       properties: {
+      //         style: 'cursor: pointer; display: flex; align-items: center; padding: 0 16px 0px 16px; border: 1px solid #e1e1e1; border-radius: 4px;',
+      //       },
+      //       children: [
+      //         {
+      //           type: 'element',
+      //           tagName: 'img',
+      //           properties: {
+      //             src: collapseSvg,
+      //             alt: 'collapse',
+      //             style: 'width: 1.5em; height: 1.5em;',
+      //           },
+      //           children: [],
+      //         },
+      //         {
+      //           type: 'text',
+      //           value: ` ${lineFoldStart} - ${lineFoldEnd}`,
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       type: 'element',
+      //       tagName: 'pre',
+      //       properties: {},
+      //       children: [
+      //         {
+      //           type: 'element',
+      //           tagName: 'code',
+      //           properties: {
+      //             style: 'border: 1px solid #e1e1e1; border-radius: 4px;'
+      //           },
+      //           children: foldLines,
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // });
+      // node.children.push({
+      //   type: 'element',
+      //   tagName: 'style',
+      //   properties: {},
+      //   children: [
+      //     {
+      //       type: 'text',
+      //       value: /* css */ ``,
+      //     },
+      //   ],
+      // });
+      // return node;
     },
-    postprocess(html, _options) {
-      return html;
+    tokens(tokens) {
+      console.log(tokens);
     },
+
+    postprocess: (html) => html,
   };
 }
+const _collapseSvg =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24'%3E%3Cpath fill='currentColor' d='M4 12h16v2H4zm0-3h16v2H4zm12-5l-4 4l-4-4h3V1h2v3zM8 19l4-4l4 4h-3v3h-2v-3z'/%3E%3C/svg%3E";
--- packages/transformers/src/utilities/index.ts
+++ packages/transformers/src/utilities/index.ts
@@ -1,4 +1,18 @@
 /**
+ * given an array and a start and end index, return 3 arrays:
+ * - the elements before the start index,
+ * - the elements between the start and end index, and
+ * - the elements after the end index
+ */
+export function splicer<T>(
+  array: Array<T>,
+  start: number,
+  end: number,
+): [Array<T>, Array<T>, Array<T>] {
+  return [array.slice(0, start), array.slice(start, end), array.slice(end)];
+}
+
+/**
  * matches and removes leading and trailing whitespace and newlines
  */
 const whitespaceRegEx = /\s*\n\s*/g;
