5c06b4090658282c9a4dbea6093e9ed899177570
omar <omaraziz.dev@proton.me>
2024-05-13 20:14:20 -0600

    chore: update dependencies (#214)

--- examples/astro/astro.config.ts
+++ examples/astro/astro.config.ts
@@ -1,7 +1,7 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import rehypePrettyCode from 'rehype-pretty-code';
+import { rehypePrettyCode } from 'rehype-pretty-code';
 import moonlightTheme from './public/theme/moonlight-ii.json';
 import { transformerCopyButton } from '@rehype-pretty/transformers';
 
