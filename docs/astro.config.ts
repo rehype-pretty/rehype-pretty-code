22a66403f7c2a3cfb23bab4bdfbf93a1faeca670
omar <omaraziz.dev@proton.me>
2024-06-27 21:50:49 -0700

    chore: reduce copy feedback

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -59,7 +59,7 @@ export default defineConfig({
           transformers: [
             transformerCopyButton({
               visibility: 'hover',
-              feedbackDuration: 2_500,
+              feedbackDuration: 2_000,
             }),
           ],
         },
